2143834f5782c47e7479b20d8048fe36a3e8cd46
ashraffares <ashraffares090@gmail.com>
2021-09-08 14:03:52 +0200

    Add two component Poke PokeList

--- src/container/App/App.js
+++ src/container/App/App.js
@@ -1,12 +1,14 @@
-import {  Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+import Poke from '../../component/Poke';
+import PokeList from '../../component/PokeList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/" component={} exact />
-        <Route path="/pokemon/:pokemon" component={} />
+        <Route path="/" component={PokeList} exact />
+        <Route path="/pokemon/:pokemon" component={Poke} />
       </Switch>
     </div>
   );
