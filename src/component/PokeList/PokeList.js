d83f07453a3e24a35ca02a5979829bba4fc5a928
ashraffares <ashraffares090@gmail.com>
2021-09-08 15:24:19 +0200

    Dispatch the FetchPokeData and get the state using useSelector hook

--- src/component/PokeList/PokeList.js
+++ src/component/PokeList/PokeList.js
@@ -1,5 +1,18 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import FetchPokeData from '../../Redux/actions/PokeAction';
 
-const PokeList = () => <div>PokeList</div>;
+const PokeList = () => {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+
+  useEffect(() => {
+    dispatch(FetchPokeData());
+  }, []);
+
+  console.log(state);
+  return <div>PokeList</div>;
+};
 
 export default PokeList;
