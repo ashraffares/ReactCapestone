1047a06b8556fe61d1e19e91e946c4243809e2c8
ashraffares <ashraffares090@gmail.com>
2021-09-08 18:39:54 +0200

    Display a list of items with name and picture

--- src/component/Poke/Poke.js
+++ src/component/Poke/Poke.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
-const Poke = () => <div>Poke</div>;
+const Poke = ({ pokemon, id }) => {
+  const { name } = pokemon;
+  return (
+    <div>
+      <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${id}.png`} alt={name} />
+      <span>{name}</span>
+    </div>
+  );
+};
+
+Poke.propTypes = {
+  pokemon: propTypes.shape({
+    name: propTypes.string.isRequired,
+    url: propTypes.string.isRequired,
+  }).isRequired,
+  id: propTypes.number.isRequired,
+};
 
 export default Poke;
--- src/component/PokeList/PokeList.js
+++ src/component/PokeList/PokeList.js
@@ -2,6 +2,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import FetchPokeData from '../../Redux/actions/PokeAction';
+import Poke from '../Poke';
 import styles from './PokeList.module.css';
 
 const PokeList = () => {
@@ -19,7 +20,7 @@ const PokeList = () => {
     const ListLength = state.items.length;
     if (ListLength > 0) {
       const pokemons = state.items;
-      return pokemons.map((poke) => <p key={poke.url}>{poke.name}</p>);
+      return pokemons.map((poke, id) => <Poke key={poke.url} id={id + 1} pokemon={poke} />);
     }
     return <h1>Can not get pokemon list try again.</h1>;
   };
