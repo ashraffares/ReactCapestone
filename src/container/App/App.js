725d1837b30a5a23e11f2a830b820cd0c51b463c
ashraffares <ashraffares090@gmail.com>
2021-09-08 20:25:27 +0200

    Add navbar to app component

--- src/container/App/App.js
+++ src/container/App/App.js
@@ -1,11 +1,13 @@
 import { Switch, Route } from 'react-router-dom';
 import Poke from '../../component/Poke';
 import PokeList from '../../component/PokeList';
+import Navbar from '../../component/navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Switch>
         <Route path="/" component={PokeList} exact />
         <Route path="/pokemon/:pokemon" component={Poke} />
