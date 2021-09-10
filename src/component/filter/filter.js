44fc2a4ca3b243025e70ac43ae1a59675be23e5a
ashraffares <ashraffares090@gmail.com>
2021-09-10 12:21:13 +0200

    Add filter component

--- src/component/filter/filter.js
+++ src/component/filter/filter.js
@@ -1,15 +1,17 @@
+/* eslint-disable no-console */
 import React from 'react';
+import styles from './filter.module.css';
 
 const Filter = () => (
-  <>
+  <div className={styles.container}>
     <label htmlFor="filter">
-      <select className="select" name="filter" id="filter">
+      <select className={styles.select} name="filter" id="filter" onChange={(e) => console.log(e.target.value)}>
         <option value="All">All</option>
         <option value="Width">Width</option>
         <option value="Height">Height</option>
       </select>
     </label>
-  </>
+  </div>
 );
 
 export default Filter;
