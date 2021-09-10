86327ab3f917e8d0413e3e9d673697acffbcfd00
ashraffares <ashraffares090@gmail.com>
2021-09-10 22:54:06 +0200

    rRmove dep redux-logger and redux-devtools-extension to seecure the state

--- src/Redux/store/store.js
+++ src/Redux/store/store.js
@@ -1,13 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import { composeWithDevTools } from 'redux-devtools-extension';
+
 import RootReducer from '../reducers/RootReducer';
 
 const store = createStore(
   RootReducer,
-  composeWithDevTools(applyMiddleware(thunk, logger)),
-
+  applyMiddleware(thunk),
 );
 
 export default store;
