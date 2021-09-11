8ef0abb8a02302d0264ee94b984ead0c0ef0843e
ashraffares <ashraffares090@gmail.com>
2021-09-11 10:41:34 +0200

    Push to github pages

--- src/container/App/App.js
+++ src/container/App/App.js
@@ -1,4 +1,6 @@
-import { Switch, Route, Redirect } from 'react-router-dom';
+import {
+  Switch, Route, Redirect, Router,
+} from 'react-router-dom';
 import PokeInfo from '../../component/PokeInfo';
 import PokeList from '../../component/PokeList';
 import Navbar from '../../component/navbar';
@@ -7,12 +9,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Switch>
-        <Route path="/" component={PokeList} exact />
-        <Route path="/pokemon/:pokemon" component={PokeInfo} />
-        <Redirect to="/" />
-      </Switch>
+      <Router basename={process.env.PUBLIC_URL}>
+        <Navbar />
+        <Switch>
+          <Route path="/" component={PokeList} exact />
+          <Route path="/pokemon/:pokemon" component={PokeInfo} />
+          <Redirect to="/" />
+        </Switch>
+      </Router>
     </div>
   );
 }
