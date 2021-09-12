0de80a3c8de38e7abb66d2bef036978cc616945d
ashraffares <ashraffares090@gmail.com>
2021-09-12 15:04:01 +0200

    Fix failing test cases

--- src/test/pokereducer.test.js
+++ src/test/pokereducer.test.js
@@ -24,3 +24,13 @@ test('Should return object with error: "error"', () => {
 
   expect(res).toEqual({ items: [], loading: false, error: 'error' });
 });
+
+test('Should return object with items have object', () => {
+  const res = PokeReducer(initialState,
+    {
+      type: 'FETCH_POKE_SUCCESS',
+      payload: { name: 'bublasaur', url: '/bulbasaur/1' },
+    });
+
+  expect(res).toEqual({ items: { name: 'bublasaur', url: '/bulbasaur/1' }, loading: false, error: null });
+});
